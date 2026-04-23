d1511329feec659d53c1dacf5744b7f62466d386
x222 <113437965+rsnx222@users.noreply.github.com>
2026-04-23 08:08:21 +0100

    Reset

--- config/puzzles.js
+++
@@ -1,9 +0,0 @@
-// puzzles.js
-
-module.exports = {
-  'puzzle_event_1': {
-    question: 'What walks on four legs in the morning, two legs at noon, and three legs in the evening?',
-    answer: 'man', // The correct answer
-  },
-  // Add more puzzles as needed
-};
